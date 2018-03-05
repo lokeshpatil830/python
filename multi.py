2e2c6290b6d8cff96601b1766aaf6cfb077e6cc2
lokeshpatil830 <lokeshpatil830@gmail.com>
2018-03-05 12:19:15 +0530

    first commit

---
+++ multi.py
@@ -0,0 +1,3 @@
+num=input("Enter a number:")
+for i in range(1,11):
+     print str(num),"x",str(i),"=",str(num*i)
---
+++ stud.py
@@ -0,0 +1,7 @@
+n=input("Enter the no of student")
+student_data=["stud_name","roll_numb","stud_class"]
+
+for i range(0, 10)
+
+ 
+stud_name=input
